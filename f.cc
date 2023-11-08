832fe6d5f0234790fdf2e5f052857bb0ab830ba4
Attila Jeges <attila.jeges@gmail.com>
2023-11-08 21:45:16 +0100

    write out sortedf files

--- f.cc
+++ f.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 #include <seastar/core/sharded.hh>
 #include <seastar/core/sstring.hh>
 #include <seastar/core/smp.hh>
@@ -37,7 +38,7 @@ struct record_type {
 future<> read_file(sstring filename, temporary_buffer<char>& rbuf, uint64_t offset, uint64_t chunk_size) {
     return with_file(open_file_dma(filename, open_flags::ro), [&rbuf, offset, chunk_size] (file& f) {
         return f.dma_read(offset, rbuf.get_write(), chunk_size).then([&rbuf, chunk_size] (size_t count) {
-            fmt::print("    read count {}\n", count);
+            fmt::print("    read count {} chunk_size {}\n", count, chunk_size);
             assert(count == chunk_size);
             return make_ready_future<>();
         });
@@ -45,7 +46,8 @@ future<> read_file(sstring filename, temporary_buffer<char>& rbuf, uint64_t offs
 }
 
 future<> write_file(sstring filename, temporary_buffer<char>& rbuf, uint64_t size) {
-    return with_file(open_file_dma(filename, open_flags::wo | open_flags::create), [&rbuf, size] (file& f) {
+    open_flags wflags = open_flags::wo | open_flags::truncate | open_flags::create;
+    return with_file(open_file_dma(filename, wflags), [&rbuf, size] (file& f) {
         return f.dma_write(0, rbuf.get(), size).then([&rbuf, size] (size_t count) {
             fmt::print("    write count {}\n", count);
             assert(count == size);
@@ -56,9 +58,9 @@ future<> write_file(sstring filename, temporary_buffer<char>& rbuf, uint64_t siz
 
 future<> read_and_sort(sstring filename, uint64_t offset, uint64_t chunk_size) {
     fmt::print("read_and_sort");
-    fmt::print(": smp::count {}\n", smp::count);
-    fmt::print(": smp::this_shard_id() {}\n", this_shard_id());
-    fmt::print(": filename {} offset {} chunk_size {}\n", filename, offset, chunk_size);
+    fmt::print("-------- smp::count {}\n", smp::count);
+    fmt::print("-------- this_shard_id() {}\n", this_shard_id());
+    fmt::print("-------- filename {} offset {} chunk_size {}\n", filename, offset, chunk_size);
 
     // Thread approach:
     // return async([filename] () {
@@ -71,10 +73,14 @@ future<> read_and_sort(sstring filename, uint64_t offset, uint64_t chunk_size) {
     auto rbuf = temporary_buffer<char>::aligned(aligned_size, chunk_size);
     return do_with(std::move(rbuf), [filename, offset, chunk_size](auto &rbuf) {
             return read_file(filename, rbuf, offset, chunk_size).then([&rbuf, chunk_size] {
-                    record_type *record_buffer = reinterpret_cast<record_type*>(rbuf.get(), rbuf.size());
+                    record_type *record_buffer = reinterpret_cast<record_type*>(rbuf.get_write());
                     std::sort(record_buffer,
                         record_buffer + chunk_size / record_size);
-                }).then([&rbuf] {
+                }).then([filename, &rbuf, chunk_size] {
+                    std::ostringstream os;
+                    os << filename << "-" << this_shard_id();
+                    fmt::print("out_filename {} \n", os.str());
+                    return write_file(os.str(), rbuf, chunk_size);
                 });
         });
 }
@@ -90,14 +96,16 @@ future<> external_merge(sstring filename, uint64_t size, uint64_t max_buffer_siz
         uint64_t offset = 0;
         for (uint64_t shard_id = 0; (shard_id < smp::count && offset < size); ++shard_id) {
             uint64_t chunk_size = (offset + max_byte_count_per_shard <= size) ? max_byte_count_per_shard : (size - offset);
-            if (chunk_size % record_size == 0) {
-              futures.push_back(
-                  smp::submit_to(0, smp_submit_to_options(), [filename, offset, chunk_size] {
-                          return read_and_sort(filename, offset, chunk_size);
-                      }));
-            }
+            chunk_size = chunk_size / record_size * record_size;
+            if (chunk_size == 0)
+                break;
+
+            futures.push_back(
+                smp::submit_to(shard_id, smp_submit_to_options(), [filename, offset, chunk_size] {
+                        return read_and_sort(filename, offset, chunk_size);
+                    }));
             offset +=  chunk_size;
-        }
+        } 
         return when_all_succeed(std::move(futures));
     }
     return make_ready_future<>();
